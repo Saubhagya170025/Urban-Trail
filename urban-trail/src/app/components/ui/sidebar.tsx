b6aece5ec8f80568834e266d331c17635becbfde
Sonali <sonalisingh.singh1299@gmail.com>
2025-05-17 02:15:01 +0530

    removed odd toggle button

--- urban-trail/src/app/components/ui/sidebar.tsx
+++ urban-trail/src/app/components/ui/sidebar.tsx
@@ -261,21 +261,22 @@ function SidebarTrigger({
   const { toggleSidebar } = useSidebar()
 
   return (
-    <Button
-      data-sidebar="trigger"
-      data-slot="sidebar-trigger"
-      variant="ghost"
-      size="icon"
-      className={cn("size-7", className)}
-      onClick={(event) => {
-        onClick?.(event)
-        toggleSidebar()
-      }}
-      {...props}
-    >
-      <PanelLeftIcon />
-      <span className="sr-only">Toggle Sidebar</span>
-    </Button>
+    <></>
+    // <Button
+    //   data-sidebar="trigger"
+    //   data-slot="sidebar-trigger"
+    //   variant="ghost"
+    //   size="icon"
+    //   className={cn("size-7", className)}
+    //   onClick={(event) => {
+    //     onClick?.(event)
+    //     toggleSidebar()
+    //   }}
+    //   {...props}
+    // >
+    //   <PanelLeftIcon />
+    //   <span className="sr-only">Toggle Sidebar</span>
+    // </Button>
   )
 }
 
